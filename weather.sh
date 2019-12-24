718d4c85a7db706c40c187ab94fa4583a5e9cdf4
daief <1437931235@qq.com>
2019-12-24 17:15:47 +0800

    refactor: update bash

--- weather.sh
+++ weather.sh
@@ -2,7 +2,7 @@
 # @Author: daief
 # @LastEditors: daief
 # @Date: 2019-12-24 16:06:57
-# @LastEditTime: 2019-12-24 16:50:55
+# @LastEditTime: 2019-12-24 17:15:29
 # @Description:
 ###
 #!/bin/sh
@@ -10,7 +10,7 @@
 set -eux
 
 # 获取到 curl 的结果
-WEATHER=$(curl "wttr.in/Hangzhou+Jiangcun?lang=zh-CN&format=4")
+WEATHER=$(curl "wttr.in/Hangzhou+Jiangcun?lang=zh-CN&format=4&m")
 
 curl $1 \
   -H 'Content-Type: application/json' \
