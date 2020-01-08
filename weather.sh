4b468b907a7cccbdc6fa5f6e8c5210fd07528e13
daief <1437931235@qq.com>
2020-01-08 19:45:58 +0800

    fix

--- weather.sh
+++ weather.sh
@@ -2,12 +2,14 @@
 # @Author: daief
 # @LastEditors: daief
 # @Date: 2019-12-24 16:06:57
-# @LastEditTime: 2020-01-08 19:26:26
+# @LastEditTime: 2020-01-08 19:45:28
 # @Description:
 ###
 #!/bin/sh
 
 set -eux
+
+input=$1
 
 # 获取到 curl 的结果
 WEATHER=$(curl "wttr.in/Hangzhou+Jiangcun?lang=zh-CN&format=4&m")
@@ -15,17 +17,9 @@ WEATHER=$(curl "wttr.in/Hangzhou+Jiangcun?lang=zh-CN&format=4&m")
 # 获取诗句，ref: https://v1.jinrishici.com
 VERSE=$(curl "https://v1.jinrishici.com/shuqing.txt")
 
-# curl $1 \
-#   -H 'Content-Type: application/json' \
-#   -d "{
+URLS=(${input//,/ })
 
-#     \"msgtype\": \"text\",
-#     \"text\": {
-#       \"content\": \"$VERSE\n$WEATHER\"
-#     }
-#   }"
-
-for url in $1; do
+for url in ${URLS[@]}; do
   curl $url \
     -H 'Content-Type: application/json' \
     -d "{
