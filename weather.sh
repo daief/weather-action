d0591b7ec1511176307c76b5ac37ae92e3bad018
daief <1437931235@qq.com>
2020-01-08 10:36:18 +0800

    feat: add fetch random verse

--- weather.sh
+++ weather.sh
@@ -2,7 +2,7 @@
 # @Author: daief
 # @LastEditors: daief
 # @Date: 2019-12-24 16:06:57
-# @LastEditTime: 2019-12-24 17:15:29
+# @LastEditTime: 2020-01-08 10:36:15
 # @Description:
 ###
 #!/bin/sh
@@ -12,11 +12,14 @@ set -eux
 # 获取到 curl 的结果
 WEATHER=$(curl "wttr.in/Hangzhou+Jiangcun?lang=zh-CN&format=4&m")
 
+# 获取诗句，ref: https://v1.jinrishici.com
+VERSE=$(curl "https://v1.jinrishici.com/shuqing.txt")
+
 curl $1 \
   -H 'Content-Type: application/json' \
   -d "{
     \"msgtype\": \"text\",
     \"text\": {
-      \"content\": \"$WEATHER\"
+      \"content\": \"$VERSE\n$WEATHER\"
     }
   }"
