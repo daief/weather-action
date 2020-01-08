b54fa8b2f53aae8472c7d0063e9b5743c0168794
daief <1437931235@qq.com>
2020-01-08 19:26:46 +0800

    refactor: try multiple url

--- weather.sh
+++ weather.sh
@@ -2,7 +2,7 @@
 # @Author: daief
 # @LastEditors: daief
 # @Date: 2019-12-24 16:06:57
-# @LastEditTime: 2020-01-08 10:36:15
+# @LastEditTime: 2020-01-08 19:26:26
 # @Description:
 ###
 #!/bin/sh
@@ -15,11 +15,24 @@ WEATHER=$(curl "wttr.in/Hangzhou+Jiangcun?lang=zh-CN&format=4&m")
 # 获取诗句，ref: https://v1.jinrishici.com
 VERSE=$(curl "https://v1.jinrishici.com/shuqing.txt")
 
-curl $1 \
-  -H 'Content-Type: application/json' \
-  -d "{
+# curl $1 \
+#   -H 'Content-Type: application/json' \
+#   -d "{
+
+#     \"msgtype\": \"text\",
+#     \"text\": {
+#       \"content\": \"$VERSE\n$WEATHER\"
+#     }
+#   }"
+
+for url in $1; do
+  curl $url \
+    -H 'Content-Type: application/json' \
+    -d "{
+
     \"msgtype\": \"text\",
     \"text\": {
       \"content\": \"$VERSE\n$WEATHER\"
     }
   }"
+done
